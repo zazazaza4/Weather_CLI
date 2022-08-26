909769ded79ebdd81ba6e2f6ee7e4a097472f0f1
zazazaza4 <6715414@gmail.com>
2022-08-26 21:17:40 +0300

    Обработка ошибок

--- weather.js
+++ weather.js
@@ -19,6 +19,21 @@ const saveToken = async (token) => {
   }
 };
 
+const getForcast = async () => {
+  try {
+    const weather = await getWeather(process.env.CITY);
+    console.log(weather);
+  } catch (error) {
+    if (error?.response?.status == 404) {
+      printError('Неверно указан город');
+    } else if (error?.response?.status == 401) {
+      printError('Неверно указан токен');
+    } else {
+      printError(error.message);
+    }
+  }
+};
+
 const initCLI = () => {
   const args = getArgs(process.argv);
 
@@ -33,7 +48,7 @@ const initCLI = () => {
     return saveToken(args.t);
   }
 
-  getWeather('london');
+  getForcast();
 };
 
 initCLI();
